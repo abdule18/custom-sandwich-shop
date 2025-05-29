7eb43db53637cc17e98ae836bb42b1974df50e27
abdule18 <101951085+abdule18@users.noreply.github.com>
2025-05-29 15:00:01 -0400

    Finished with the minimum requirment

--- src/main/java/com/pluralsight/userinterface/UserInterFace.java
+++ src/main/java/com/pluralsight/userinterface/UserInterFace.java
@@ -20,7 +20,7 @@ public class UserInterFace {
         displayHelper();
     }
 
-    public void displayHelper(){
+    private void displayHelper(){
         String promptUser =
                 "1. Home Screen\n" +
                         "2. Order Screen\n" +
@@ -63,7 +63,7 @@ public class UserInterFace {
         } while (option != 0);
     }
 
-    public void showHomeScreen(){
+    private void showHomeScreen(){
         String promptUser =
                 "1. New Order\n" +
                 "0. Exit\n";
@@ -88,7 +88,7 @@ public class UserInterFace {
 
     }
 
-    public void showOrderScreen(){
+    private void showOrderScreen(){
         String promptUser =
                         "1. Add a Sandwich\n" +
                         "2. Add a Drink\n" +
@@ -122,7 +122,7 @@ public class UserInterFace {
 
         } while (option != 0);
     }
-    public void addSandwich(){
+    private void addSandwich(){
         System.out.println("=== Add a Sandwich ===");
 
         // Choosing Sandwich Size
@@ -154,110 +154,205 @@ public class UserInterFace {
         Sandwich sandwich  = new Sandwich("Sandwich", size, bread);
         sandwich.setToasted(toasted);
 
-
-//         Add Cheese Toppings
-//        while (true) {
-//            String addCheese = console.promptForString("Add a cheese topping? (yes/no): ");
-//            if (!addCheese.equalsIgnoreCase("yes")) break;
-//
-//            String cheeseName = console.promptForString("Enter cheese (American, Provolone, Cheddar, Swiss): ");
-//            String extraInput = console.promptForString("Do you want extra cheese? (yes/no): ");
-//            boolean isExtra = extraInput.equalsIgnoreCase("yes");
-//           // if(validateCheese(cheeseName)){
-//                sandwich.addPremiumTopping(cheeseName, isExtra);
-//           // }
-//           // else{
-//           //     System.out.println( "not a valid cheese option");
-//           // }
-//
-//        }
-
         addCheeseToppings(sandwich);
         addMeatToppings(sandwich);
         addRegularToppings(sandwich);
+        addSauces(sandwich);
 
+        System.out.println();
         System.out.println("Sandwich added  to your order:");
         System.out.println(sandwich.getDescription());
         System.out.println("Total Price: $" + String.format("%.2f", sandwich.calculatePrice()));
+        System.out.println();
 
         order.addSandwich(sandwich);
 
     }
 
-
     private void addCheeseToppings(Sandwich sandwich) {
-        while (true) {
-            String addCheese = console.promptForString("Add a cheese topping? (yes/no): ");
-            if (!addCheese.equalsIgnoreCase("yes")) break;
+        String addCheese = console.promptForString("Would you like to add cheese to your sandwich? (yes/no): ");
+        if (!addCheese.equalsIgnoreCase("yes")) {
+            return; // Skip cheese section
+        }
 
-            String cheeseName = console.promptForString("Enter cheese (American, Provolone, Cheddar, Swiss): ");
-            String extraInput = console.promptForString("Do you want extra cheese? (yes/no): ");
-            boolean isExtra = extraInput.equalsIgnoreCase("yes");
+        String[] cheeseOptions = {"American", "Provolone", "Cheddar", "Swiss"};
+        boolean addMore = true;
 
-            // Add the cheese topping to the sandwich
-            sandwich.addPremiumTopping(cheeseName, isExtra);
+        while (addMore) {
+            System.out.println("\n--- Cheese Options ---");
+            int index = 1;
+            for (String cheese : cheeseOptions) {
+                System.out.println(index++ + ". " + cheese);
+            }
+
+            int choice = console.promptForInt("Select the corresponding number: ");
+            if (choice >= 1 && choice <= cheeseOptions.length) {
+                String selectedCheese = cheeseOptions[choice - 1];
+                String extraInput = console.promptForString("Do you want extra " + selectedCheese + "? (yes/no): ");
+                boolean isExtra = extraInput.equalsIgnoreCase("yes");
+                sandwich.addPremiumTopping(selectedCheese, isExtra);
+                System.out.println("Adding " + selectedCheese + (isExtra ? " (Extra)" : ""));
+            } else {
+                System.out.println("Invalid choice.");
+            }
+
+            int more = console.promptForInt("Do you want to add another cheese? (1-Yes, 2-No): ");
+            addMore = (more == 1);
         }
     }
 
     private void addMeatToppings(Sandwich sandwich) {
-        while (true) {
-            String addMeat = console.promptForString("Add a meat topping? (yes/no): ");
-            if (!addMeat.equalsIgnoreCase("yes")) break;
+        String addMeat = console.promptForString("Would you like to add Meat Topping to your sandwich? (yes/no): ");
+        if (!addMeat.equalsIgnoreCase("yes")) {
+            return; // Skip cheese section
+        }
+        String[] meatOptions = {"Steak", "Ham", "Salami", "Roast Beef", "Chicken", "Bacon"};
+        boolean addMore = true;
 
-            String meatName = console.promptForString("Enter meat (Ham, Turkey, Chicken, etc.): ");
-            String extraInput = console.promptForString("Do you want extra meat? (yes/no): ");
-            boolean isExtra = extraInput.equalsIgnoreCase("yes");
+        while (addMore) {
+            System.out.println("\n--- Meat Options ---");
+            int index = 1;
+            for (String meat : meatOptions) {
+                System.out.println(index++ + ". " + meat);
+            }
 
-            // Add the meat topping to the sandwich
-            sandwich.addPremiumTopping(meatName, isExtra);
+            int choice = console.promptForInt("Select the corresponding number: ");
+            if (choice >= 1 && choice <= meatOptions.length) {
+                String selectedMeat = meatOptions[choice - 1];
+                String extraInput = console.promptForString("Do you want extra " + selectedMeat + "? (yes/no): ");
+                boolean isExtra = extraInput.equalsIgnoreCase("yes");
+                sandwich.addPremiumTopping(selectedMeat, isExtra);
+                System.out.println("Adding " + selectedMeat + (isExtra ? " (Extra)" : ""));
+            } else {
+                System.out.println("Invalid choice.");
+            }
+
+            int more = console.promptForInt("Do you want to add another meat? (1-Yes, 2-No): ");
+            addMore = (more == 1);
         }
     }
 
     private void addRegularToppings(Sandwich sandwich) {
-        while (true) {
-            String addRegular = console.promptForString("Add a regular topping? (yes/no): ");
-            if (!addRegular.equalsIgnoreCase("yes")) break;
+        String addVegetable = console.promptForString("Would you like to add Vegetable to your sandwich? (yes/no): ");
+        if (!addVegetable.equalsIgnoreCase("yes")) {
+            return; // Skip cheese section
+        }
+        String[] regularOptions = {
+                "Lettuce", "Peppers", "Onions", "Tomatoes", "Jalapeños",
+                "Cucumbers", "Pickles", "Guacamole", "Mushrooms"
+        };
 
-            String toppingName = console.promptForString("Enter topping name (e.g., Lettuce, Tomato): ");
+        boolean addMore = true;
 
-            // Add the regular topping to the sandwich
-            sandwich.addRegularTopping(toppingName);
+        while (addMore) {
+            System.out.println("\n--- Regular Topping Options ---");
+            int index = 1;
+            for (String topping : regularOptions) {
+                System.out.println(index++ + ". " + topping);
+            }
+
+            int choice = console.promptForInt("Select the corresponding number: ");
+            if (choice >= 1 && choice <= regularOptions.length) {
+                String selectedTopping = regularOptions[choice - 1];
+                sandwich.addRegularTopping(selectedTopping);
+                System.out.println("Adding " + selectedTopping);
+            } else {
+                System.out.println("Invalid choice.");
+            }
+
+            int more = console.promptForInt("Do you want to add another topping? (1-Yes, 2-No): ");
+            addMore = (more == 1);
         }
     }
 
+    private void addSauces(Sandwich sandwich) {
+        String addSauce = console.promptForString("Would you like to add any Sauce? (yes/no): ");
+        if (!addSauce.equalsIgnoreCase("yes")) {
+            return; // Skip cheese section
+        }
+        String[] sauceOptions = {"Mayo", "Mustard", "Ketchup", "Ranch", "Thousand Islands", "Vinaigrette"};
+        boolean addMore = true;
+
+        while (addMore) {
+            System.out.println("\n--- Sauce Options ---");
+            int index = 1;
+            for (String sauce : sauceOptions) {
+                System.out.println(index++ + ". " + sauce);
+            }
+
+            int choice = console.promptForInt("Select the corresponding number: ");
+            if (choice >= 1 && choice <= sauceOptions.length) {
+                String selectedSauce = sauceOptions[choice - 1];
+                sandwich.addRegularTopping(selectedSauce);
+                System.out.println("✅ Adding " + selectedSauce);
+            } else {
+                System.out.println("Invalid choice.");
+            }
+
+            int more = console.promptForInt("Do you want to add another sauce? (1-Yes, 2-No): ");
+            addMore = (more == 1);
+        }
+    }
 
     public void addDrink(){
         System.out.println("=== Add a Drink ===");
-        System.out.println("Choose a drink size: ");
-        DrinkSize[]  sizes = DrinkSize.values();
+        DrinkSize[] sizes = DrinkSize.values();
         int index = 1;
-        for (DrinkSize size: sizes) {
+        System.out.println("\n--- Drink Sizes ---");
+        for (DrinkSize size : sizes) {
             System.out.println(index++ + ". " + size);
         }
 
-        int sizeChoice = console.promptForInt("Enter Choice: ");
+        int sizeChoice = console.promptForInt("Select drink size: ");
+        if (sizeChoice < 1 || sizeChoice > sizes.length) {
+            System.out.println("Invalid size choice.");
+            return;
+        }
+
         DrinkSize selectedSize = sizes[sizeChoice - 1];
 
-        String flavor = console.promptForString("Enter drink flavor (e.g., Coke, Sprite, Orange): ");
+        // Drink flavor options
+        String[] flavorOptions = {"Coke", "Sprite", "Fanta", "Orange Juice", "Lemonade"};
+        index = 1;
+        System.out.println("\n--- Drink Flavors ---");
+        for (String flavor : flavorOptions) {
+            System.out.println(index++ + ". " + flavor);
+        }
 
-        Drink drink = new Drink(flavor + "Drink", selectedSize, flavor);
+        int flavorChoice = console.promptForInt("Select drink flavor: ");
+        if (flavorChoice < 1 || flavorChoice > flavorOptions.length) {
+            System.out.println("Invalid flavor choice.");
+            return;
+        }
 
+        String selectedFlavor = flavorOptions[flavorChoice - 1];
+
+        Drink drink = new Drink(selectedFlavor + " Drink", selectedSize, selectedFlavor);
         order.addProduct(drink);
-
-        System.out.println("Drink added: " + drink.getDescription());
+        System.out.println("✅ " + selectedSize + " " + selectedFlavor + " added.");
         System.out.println("Price: $" + String.format("%.2f", drink.calculatePrice()));
 
     }
     public void addChips(){
         System.out.println("=== Add a Chips ===");
 
-        String chipsType  = console.promptForString("Enter chips type  (e.g., Doritos, Lays, BBQ, etc.): ");
+        String[] chipTypes = {"Doritos", "Lays", "Cheetos", "Sun Chips", "BBQ", "Salt & Vinegar"};
+        int index = 1;
+        System.out.println("\n--- Chips Options ---");
+        for (String chip : chipTypes) {
+            System.out.println(index++ + ". " + chip);
+        }
 
-        Chips chips = new Chips(chipsType + "Chips", chipsType);
+        int chipChoice = console.promptForInt("Select your chip: ");
+        if (chipChoice < 1 || chipChoice > chipTypes.length) {
+            System.out.println("Invalid chip choice.");
+            return;
+        }
 
+        String selectedChip = chipTypes[chipChoice - 1];
+        Chips chips = new Chips(selectedChip + " Chips", selectedChip);
         order.addProduct(chips);
-
-        System.out.println("Chips added: " + chips.getDescription());
+        System.out.println("✅ " + selectedChip + " added.");
         System.out.println("Price: $" + String.format("%.2f", chips.calculatePrice()));
     }
     public void checkout(){
